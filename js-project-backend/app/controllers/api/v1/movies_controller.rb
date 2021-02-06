8038b2b3d51145d8cbe95f2cf951eadc280709f5
khargr14 <kay_hargrove@yahoo.com>
2021-02-06 17:11:49 -0500

    create routes

--- js-project-backend/app/controllers/api/v1/movies_controller.rb
+++ js-project-backend/app/controllers/api/v1/movies_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::MoviesController < ApplicationController
+
+    def index
+        movies = Movie.all
+        render json: movies
+    end
+
+    def show 
+        movie = Movie.find(params[:id])
+        render json: movies
+    end
+
+
+    def create
+        movie = Movie.create(movie_params)
+        movie.save!
+        render json: movie
+    end 
+
+    def update
+        movie = Movie.find(params[:id])
+        movie.update(movie_params)
+        render json: movie
+    end
+
+    def destroy 
+        movie = Movie.find_by(:id => params[:id]).destroy
+        render json: movie
+    end
+
+    private
+        def movie_params
+            params.require(:movie).permit(:title, :director, :summary, :review, :rating, :user_id )
+        end
+
+
 end
