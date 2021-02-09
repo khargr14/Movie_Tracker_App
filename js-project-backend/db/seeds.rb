41102ebc6e55d67024d65f19f94dcc89370b5cc6
khargr14 <kay_hargrove@yahoo.com>
2021-02-09 15:51:23 -0500

    seed data displaying

--- js-project-backend/db/seeds.rb
+++ js-project-backend/db/seeds.rb
@@ -12,11 +12,11 @@ movies = Movie.create([
     { title:  'Outside the Wire', summary: 'In the near future, a drone pilot sent into a war zone finds himself paired with a top-secret android officer on a mission to stop a nuclear attack.',
               director: 'Damson Idris', review: ' This movie kept me on my toes', rating: '5', user_id: 1 },
     { title:  'The Secret Life of Pets 2', summary: 'On a farm outside New York, Max aims to boost his confidence while in the city, Snowball attempts to rescue a tiger cub and Gidget pretends to be a cat.',
-              director: 'Kevin Hart', review: 'This is a cute movie for kids', rating: '3', user_id: 2},
+              director: 'Kevin Hart', review: 'This is a cute movie for kids', rating: '3', user_id: 1},
     { title: 'Good Burger', summary: 'Working at a neighborhood fast-food joint, two teens try to save the restaurant when a giant burger franchise fires up the competition.',
-              director: 'Kel Mitchell', review: 'This movie is good to watch to kill time', rating: '2', user_id: 2},
+              director: 'Kel Mitchell', review: 'This movie is good to watch to kill time', rating: '2', user_id: 1},
     { title: 'Cloudy with a Chance of Meatballs 2', summary: 'Inventor Flint Lockwood battles mutant food beasts created by his notorious machine, including shrimpanzees, tacodiles and jellyfish sandwiches',
-                director: 'Bill Hader', review: 'Great family movie', rating: '3', user_id: 2}
+                director: 'Bill Hader', review: 'Great family movie', rating: '3', user_id: 1}
       
     
     ])
