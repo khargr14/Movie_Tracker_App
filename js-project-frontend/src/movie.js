702087d78603968e54fcd70cc4a970275fad2316
khargr14 <kay_hargrove@yahoo.com>
2021-02-18 00:48:32 -0500

    data is retrieve

--- js-project-frontend/src/movie.js
+++ js-project-frontend/src/movie.js
@@ -1,11 +1,30 @@
 const endPoint = "http://localhost:3000/api/v1/movies"
 
 document.addEventListener('DOMContentLoaded', () => {
+    getMov()
+    
+})
+
+function getMov(){
     fetch(endPoint)
     .then(response => response.json())
-    .then(movies => {
-        console.log(movies);
+    .then(mov => {
+      //  console.log(movies)\
+      
+      mov.data.forEach(movie  => {
+        const movieEnhancement = `
+          <div data-id=${movie.title}>
+          <h3>${movie.attributes.director}</h3>
+            <h2>${movie.attributes.summary}</h2>
+            <h4>${movie.attributes.review}</h4>
+            <p>${movie.attributes.rating}</p>
+            
+          
+          </div>
+          <br><br>`;
+
+        document.querySelector('#movie-container').innerHTML += movieEnhancement
+      })
     })
+}
 
-
-})
