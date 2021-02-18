b436d1b95a79289df40615c579f5d7dfd911b1c9
khargr14 <kay_hargrove@yahoo.com>
2021-02-18 12:56:25 -0500

    Eventlistener

--- js-project-frontend/src/movie.js
+++ js-project-frontend/src/movie.js
@@ -1,9 +1,20 @@
 const endPoint = "http://localhost:3000/api/v1/movies"
 
-document.addEventListener('DOMContentLoaded', () => {
-    getMov()
-    
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('dom content')  
+  getMov()
+  eventListen()
 })
+
+function eventListen() {
+  const createMovieForm = document.getElementById('create-movie-form')
+
+  createMovieForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    console.log('hit')
+    createButtonSubmit(e)
+  })
+}
 
 function getMov(){
     fetch(endPoint)
@@ -14,6 +25,7 @@ function getMov(){
       mov.data.forEach(movie  => {
         const movieEnhancement = `
           <div data-id=${movie.title}>
+          <h1>${movie.attributes.title}</h1>
           <h3>${movie.attributes.director}</h3>
             <h2>${movie.attributes.summary}</h2>
             <h4>${movie.attributes.review}</h4>
@@ -28,3 +40,7 @@ function getMov(){
     })
 }
 
+function createButtonSubmit(e) {
+  e.preventDefault()
+  console.log(e);
+}
