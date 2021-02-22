f7425c4b487779919a360f35894f5b1ad135a89d
khargr14 <kay_hargrove@yahoo.com>
2021-02-22 18:04:15 -0500

    fixing user backend with serializer

--- js-project-backend/app/controllers/api/v1/users_controller.rb
+++ js-project-backend/app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,26 @@
 class Api::V1::UsersController < ApplicationController
     def index
         users = User.all 
-        render json: users
-    end
-    def create 
+        #have to add the  serializer to show associated to movies
+        render json: UserSerializer.new(users).serialized_json
+    
+    def create
+        user = User.find_by(:name => user_params[:name]) 
         if User.find_by(:name => user_params[:name])
-            user = User.find_by(:name => user_params[:name])
+            
             redirect_to "/api/v1/users/#{user.id}"
         else
             user = User.create(user_params)
             user.save!
-            #render json: user
-            render json: UserSerializer.new(user)
+            render json: user
+            #render json: UserSerializer.new(user)
         end
     end
 
     def show
         user = User.find_by(:id => params[:id])
        # render json: user
-        render json: UserSerializer.new(user)
+        render json: UserSerializer.new(user).serialized_json
     end
 
     private
