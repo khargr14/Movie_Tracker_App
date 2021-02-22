511e9ef96a407b03c9a1a785d23c513d1babe89f
khargr14 <kay_hargrove@yahoo.com>
2021-02-22 17:52:33 -0500

    Creating a User fetch(Post request)

--- js-project-frontend/src/user.js
+++ js-project-frontend/src/user.js
@@ -12,3 +12,36 @@ class User {
     this.movies = user.movies;
 
 }
+
+static creatUser(User) {
+    let newUserFrom = document.getElementById('new-user-form')
+    newUserForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        fetch('http://localhost:3000/api/v1/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json'
+            },
+            body: JSON.stringify({
+                user: {
+                    name: e.target.children[1].value
+                }
+            })
+        })
+            .then(res => {
+                if(!res.ok) {
+                    throw new Error();
+                }
+                return res.json();
+            })
+            .then (user => {
+                let newUser = new User(user.data.attributes)
+                console.log(user)
+                newUser.displayUser();
+            })
+            .catch(error => {
+                console.error('User class Error', error)
+            })
+    })
+}
